16176b9722e84034d1d87ab435c7bac9c6476ae8
Palme412 <jakepalmer412@gmail.com>
2021-12-01 18:20:49 -0600

    find all

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const { Eastern_Conference, Western_Conference, Goal_Leaders, Career_Goals } = require("./models");
 
-Career_Goals.create({
-    name: 'Steve Yzerman',
-    gamesplayed: 1514,
-    goals: 692,
-    assists: 1063,
-    points: 1755
-})
-    .then(function (newTeam) {
-        console.log("New Player Added");
-        console.log(newTeam.toJSON());
+// Career_Goals.create({
+//     name: 'Steve Yzerman',
+//     gamesplayed: 1514,
+//     goals: 692,
+//     assists: 1063,
+//     points: 1755
+// })
+//     .then(function (newTeam) {
+//         console.log("New Player Added");
+//         console.log(newTeam.toJSON());
+//     })
+//     .catch(function (error) {
+//         console.log('Error creating team', error);
+//     });
+
+Eastern_Conference.findAll()
+    .then(function (EasternConferenceArray) {
+        console.log(EasternConferenceArray);
+        for (let i = 0; i < EasternConferenceArray.length; i++) {
+            let teamConference = EasternConferenceArray[i];
+            console.log("Team Conference", i);
+            console.log(teamConference.toJSON());
+        }
     })
-    .catch(function (error) {
-        console.log('Error creating team', error);
+    .catch(function (err) {
+        console.log('ERROR');
+        console.log(err);
     });
